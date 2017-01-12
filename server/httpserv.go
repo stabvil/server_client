d43c7d4e72281c13e212290877b200c24e2b40b6
stabvil <stabvil@gmail.com>
2017-01-12 19:34:47 +0300

    Used gofmt

--- server/httpserv.go
+++ server/httpserv.go
@@ -1,29 +1,23 @@
 package main
 
 import (
-	"net/http"
-	"github.com/gorilla/mux"	
 	"encoding/json"
+	"github.com/gorilla/mux"
+	"net/http"
 )
-
-
 
 func status(w http.ResponseWriter, r *http.Request) {
 
-	
-	slcB, _ := json.Marshal([]string{"server","running"})
+	slcB, _ := json.Marshal([]string{"server", "running"})
 	w.Write([]byte(slcB))
 }
 
-
-
-
 func httpserver() {
-	
+
 	r := mux.NewRouter()
 	r.HandleFunc("/status", status)
+
 	http.Handle("/", r)
-	
 	http.ListenAndServe(":8000", nil)
-	
+
 }
--- server/main.go
+++ server/main.go
@@ -7,12 +7,12 @@ import (
 
 func main() {
 	httpserv := cli.App("server", "server app")
-	
+
 	httpserv.Command("run", "raise server in 127.0.0.1:8000", func(cmd *cli.Cmd) {
 		cmd.Action = func() {
 			httpserver()
 		}
 	})
-	
+
 	httpserv.Run(os.Args)
 }
